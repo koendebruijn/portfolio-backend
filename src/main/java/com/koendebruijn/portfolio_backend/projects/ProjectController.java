8504cdee02308f522cfa02c316d97b7ca5446a5d
koendebruijn <bruijnk@gmail.com>
2021-09-20 13:28:49 +0200

    Added files

--- src/main/java/com/koendebruijn/portfolio_backend/projects/ProjectController.java
+++ src/main/java/com/koendebruijn/portfolio_backend/projects/ProjectController.java
@@ -1,13 +1,11 @@
 package com.koendebruijn.portfolio_backend.projects;
 
-import com.koendebruijn.portfolio_backend.github.GithubService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -22,7 +20,7 @@ public class ProjectController {
 
     @GetMapping
     public List<Project> getProjects() {
-       return projectService.getProjects();
+        return projectService.getProjects();
     }
 
     @GetMapping("/{id}")
