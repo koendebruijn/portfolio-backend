f297b33734767c4ce9ea94d4e30b723f7fa11ab2
koendebruijn <bruijnk@gmail.com>
2021-09-24 11:12:07 +0200

    Added javadoc

--- src/main/java/com/koendebruijn/portfolio_backend/projects/ProjectController.java
+++ src/main/java/com/koendebruijn/portfolio_backend/projects/ProjectController.java
@@ -17,16 +17,32 @@ public class ProjectController {
     private final GithubService githubService;
 
 
+    /**
+     * Get all the projects
+     *
+     * @return A list of projects
+     */
     @GetMapping
     public List<Project> getProjects() {
         return projectService.getProjects();
     }
 
+    /**
+     * Get a single project by id
+     *
+     * @param id the id of the project
+     * @return project
+     */
     @GetMapping("/{id}")
     public Project getProject(@PathVariable Long id) {
         return projectService.getProjectById(id);
     }
 
+    /**
+     * This endpoint is to manually update the repo's from github.
+     *
+     * @throws IOException could throw exception when request fails
+     */
     @GetMapping("/update")
     @ResponseStatus(value = HttpStatus.NO_CONTENT)
     public void updateProjects() throws IOException {
